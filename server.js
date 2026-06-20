3541efb360c1c40bcf2e39afc7e32feea7d3e08e
RanaUsman5421 <153462482+RanaUsman5421@users.noreply.github.com>
2026-06-20 21:43:50 +0500

    solved cors error in server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://shopify-dashboard-topaz.vercel.app",
   ];
   const origin = req.get("origin");
 
